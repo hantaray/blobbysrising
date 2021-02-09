e94ab537fbf79bb68e1e2c75eef72403e472bacd
hasashi <hasashi@e.mail.de>
2021-02-09 22:34:26 +0100

    change to camera-movement

--- Player.gd
+++ Player.gd
@@ -5,13 +5,20 @@ export (int) var speed = 400
 var velocity = Vector2()
 
 func get_input():
-    velocity = Vector2()
-    if Input.is_action_pressed('down'):
-        velocity.y += 1
-    if Input.is_action_pressed('up'):
-        velocity.y -= 1
-    velocity = velocity.normalized() * speed
+	velocity = Vector2()
+	if Input.is_action_pressed('down'):
+		if position.y <= 200:
+			velocity.y += 1
+	if Input.is_action_pressed('up'):
+		if position.y >= -226:
+			velocity.y -= 1
+	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
-    get_input()
-    velocity = move_and_slide(velocity)
+	print(position.y)
+	var curPos =  position
+	curPos.x = curPos.x + 100 * delta
+	position =  curPos
+	
+	get_input()
+	velocity = move_and_slide(velocity)
--- ScrollingBackground.gd
+++ ScrollingBackground.gd
@@ -1,19 +1,19 @@
 extends ParallaxLayer
-
-var width = 0
-
-func _ready():
-	width = $TextureRect.texture.get_width()
-	pass
-	
-func _process(delta):
-	var curPos =  position
-	curPos.x = curPos.x - 100 * delta
-	position =  curPos
-	
-	print(position.x)
-	if position.x <= -width/4:
-		position.x = 0
+#
+#var width = 0
+#
+#func _ready():
+#	width = $TextureRect.texture.get_width()
+#	pass
+#
+#func _process(delta):
+#	var curPos =  position
+#	curPos.x = curPos.x - 100 * delta
+#	position =  curPos
+#
+#	print(position.x)
+#	if position.x <= -width/4:
+#		position.x = 0
 
 ## Called when the node enters the scene tree for the first time.
 #func _ready():
