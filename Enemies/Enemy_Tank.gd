5d45f205c367daae48829c816c9e78f29b1827bc
hasashi <hasashi@e.mail.de>
2021-02-17 23:33:44 +0100

    added score, screenborderposition by playerposition

--- Enemies/Enemy_Tank.gd
+++ Enemies/Enemy_Tank.gd
@@ -7,4 +7,6 @@ extends Area2dMovement
 
 func _on_Enemy_Tank_body_entered(body):
 	if body.name == "Player_JustJump":
-		body.gameOver()
+		var main = get_node('../')
+		main.decrease_score()
+		hide()
--- main.gd
+++ main.gd
@@ -1,17 +1,27 @@
 extends Node2D
 
+var score = 0
+
 var enemyXPosOffset = 0
 var enemyTank = preload("res://Enemies/Enemy_Tank.tscn")
 var friendDove = preload("res://Friends/Friend_Dove.tscn")
 
 func _ready():
 	$EnemyTimer.start()
+	
+func increase_score():
+	score += 1
+	$HUD.update_score(score)
+	
+func decrease_score():
+	score -= 1
+	$HUD.update_score(score)
 
 func _on_EnemyTimer_timeout():
 	enemyXPosOffset += 800
 	# Choose a random location on Path2D.
 	$SpawnPath/SpawnLocation.offset = randi()
-	var mob = enemyTank.instance()
+	var mob = enemyTank.instance()  
 	if randi() % 2 == 0:
 		mob = friendDove.instance()
 	else:
