3969ca262a43623ab6ff6696aee0faa36bc95d80
Konne KLUK <konne@kombinat-luk.de>
2021-02-24 19:56:46 +0100

    change player name in levels

--- Enemies/Enemy_Tank.gd
+++ Enemies/Enemy_Tank.gd
@@ -1,12 +1,8 @@
 extends Area2dMovement
 
 
-
-
-
-
 func _on_Enemy_Tank_body_entered(body):
-	if body.name == "Player_JustJump" or body.name == "Player_UpDown":
+	if body.name == "Player":
 		var main = get_node('../')
 		main.decrease_score()
 		hide()
