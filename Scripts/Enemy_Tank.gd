3577edc6845784c02b8d4429ec1a16581a3af602
Konne KLUK <konne@kombinat-luk.de>
2021-02-24 22:14:31 +0100

    bouncyBlob implemented and some changes in main script

--- Scripts/Enemy_Tank.gd
+++ Scripts/Enemy_Tank.gd
@@ -4,5 +4,5 @@ extends Area2dMovement
 func _on_Enemy_Tank_body_entered(body):
 	if body.name == "Player":
 		var main = get_node('../')
-		main.decrease_score()
+		main.decrease_score(3)
 		hide()
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -6,6 +6,7 @@ var spawningPoints = [Vector2(2300, 100), Vector2(2300, 500), Vector2(2300, 900)
 var enemyXPosOffset = 0
 var enemyTank = preload("res://Enemies/Enemy_Tank.tscn")
 var friendDove = preload("res://Friends/Friend_Dove.tscn")
+var enemyBouncyBlob = preload("res://Enemies/Enemy_BouncyBlob.tscn")
 
 func _ready():
 	$EnemyTimer.start()
@@ -14,23 +15,29 @@ func increase_score():
 	score += 1
 	$HUD.update_score(score)
 	
-func decrease_score():
+	
+# hab hier den paramter ergänzt zum einstellen im enemy scripten
+func decrease_score(var points):
 	#if score >= 1:
-	score -= 3
+	score -= points
 	$HUD.update_score(score)
 
 func _on_EnemyTimer_timeout():
 	enemyXPosOffset += 800
 	var enemyPos = spawningPoints[randi() % 3]
 	# Choose a random location on Path2D.
-	$SpawnPath/SpawnLocation.offset = randi() % 4
-	var mob = enemyTank.instance()  
-	if randi() % 2 == 0:
+	$SpawnPath/SpawnLocation.offset = randi()
+	var mob = enemyTank.instance()
+	var rndFactorSpawn = randi() % 3
+	if rndFactorSpawn == 0:
 		mob = friendDove.instance()
-	else:
-		mob = enemyTank.instance()
+	elif rndFactorSpawn == 1:
+		mob = enemyBouncyBlob.instance()
+	# hab das letzte else rausgenommen, da unnötig mob ist ja bereits tank
+	
 	
 	add_child(mob)
 	mob.position = enemyPos
-#	mob.position = $SpawnPath/SpawnLocation.position
+	# hab die Zeile wieder angemacht, scheint jetzt wieder auf dem ganzen Pfad zu spawnen, richtig?
+	mob.position = $SpawnPath/SpawnLocation.position
 	mob.position.x += enemyXPosOffset
