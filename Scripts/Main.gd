8479dac92d51bd7fee228cd9a8a4c4e82a44c805
hasashi <hasashi@e.mail.de>
2021-03-17 22:01:22 +0100

    startscreen

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -3,7 +3,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_tree().change_scene("res://levels/JumpMovement.tscn")
+	get_tree().change_scene("res://MainMenu.tscn")
 	#feedback for scene loaded then call HUD
 
 
