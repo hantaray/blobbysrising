1867309322750d467eb7dfdf10ca99c1cdfbd522
hasashi <hasashi@e.mail.de>
2021-03-25 22:01:05 +0100

    added MobDestructionBorder

--- Scripts/Enemy_Tank.gd
+++ Scripts/Enemy_Tank.gd
@@ -3,8 +3,10 @@ extends Area2dEnemy
 
 
 func _on_Enemy_Tank_body_entered(body):
-	#if body.name == "Player":
+	if body.name == "Player":
 	#var main = get_node('../')
 	#main.decrease_score(2)
-	get_tree().call_group("HUD", "decrease_lives", life_damage)
-	queue_free()
+		get_tree().call_group("HUD", "decrease_lives", life_damage)
+		queue_free()
+	elif body.name == "MobDestructionBorder":
+		queue_free()
--- Scripts/friend_deckel.gd
+++ Scripts/friend_deckel.gd
@@ -3,6 +3,9 @@ extends Area2dFriend
 
 
 func _on_deckel_friend_body_entered(body):
-	var main = get_node('../')
-	main.increase_score()
-	queue_free()
+	if body.name == "Player":
+		var main = get_node('../')
+		main.increase_score()
+		queue_free()
+	elif body.name == "MobDestructionBorder":
+		queue_free()
