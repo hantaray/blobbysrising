f15090331ae30d21b79c9a51667bdb27e24be43f
hanti <13242996+hasashi2@users.noreply.github.com>
2021-04-29 14:17:42 +0200

    changes

---
+++ start_screen/MenuButton.gd
@@ -0,0 +1,7 @@
+extends TextureButton
+
+
+
+
+func _on_MenuButton_pressed():
+	get_tree().change_scene("res://start_screen/StartScreen.tscn")
---
+++ start_screen/buttons/InfoButton.gd
@@ -0,0 +1,7 @@
+extends Button
+
+
+
+
+func _on_InfoButton_pressed():
+	get_tree().change_scene("res://start_screen/InfoScreen.tscn")
