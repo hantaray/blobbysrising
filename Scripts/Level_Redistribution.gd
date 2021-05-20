390ed8735b2361000a3ccf705e3184f384edc9d5
hanti <13242996+hasashi2@users.noreply.github.com>
2021-05-21 00:16:57 +0200

    updated redisLevel

--- Scripts/Level_Redistribution.gd
+++ Scripts/Level_Redistribution.gd
@@ -10,7 +10,7 @@ func _enter_tree():
 	MainScript.reset_level_data()
 
 func _ready():
-	get_node("Music").play()
+#	get_node("Music").play()
 	MainScript.current_scene = "Redis"
 	GamePlayData.playerSpeed  = 900
 	randomize()
@@ -19,24 +19,23 @@ func _ready():
 
 func _on_EnemySpawnTimer_timeout():
 	$SpawnPath/SpawnLocation.offset = randi()
+	var mob = enemy_panama_bkf.instance()
+	add_child(mob)
+	mob.position = $SpawnPath/SpawnLocation.position
+	# Make sure panama is spawned where the last money was spawned
+	mob.position.x += $Player.position.x
+
+func _on_FriendSpawnTimer_timeout():
+	$SpawnPath/SpawnLocation.offset = randi()
 	var mob = friend_koffer.instance()
-	# Make shure friend is spawned at least all 5 spawns
-	if noFriendSpawnCounter < 5:
-		var rndFactorSpawn = randi() % 2
-		if rndFactorSpawn == 0:
-			mob = enemy_panama_bkf.instance()
-			noFriendSpawnCounter += 1
-	else:
-		noFriendSpawnCounter = 0
 	add_child(mob)
 	mob.position = $SpawnPath/SpawnLocation.position
 	# Make sure panama is spawned where the last money was spawned
 	mob.position.x += $Player.position.x
 	
-	# Spawn enemy right after friend
-	if mob.name == "friend_koffer":
-		prevMoneySpawn = mob.position
-		mob = enemy_panama_bkf.instance()
-		add_child(mob)
-		mob.position = prevMoneySpawn
-		mob.position.x += $Player.position.x
+#	# Spawn enemy right after friend
+#	prevMoneySpawn = mob.position
+#	mob = enemy_panama_bkf.instance()
+#	add_child(mob)
+#	mob.position = prevMoneySpawn
+#	mob.position.x += $Player.position.x
--- Scripts/friend_koffer.gd
+++ Scripts/friend_koffer.gd
@@ -6,11 +6,11 @@ var geld = preload("res://characters/friends/friend_geld.tscn")
 
 
 func _on_Timer_timeout():
-	if $VisibilityNotifier2D.is_on_screen():
-		var geld_inst = geld.instance()
-		var parent = get_parent()
-		if parent != null:
-			parent.add_child(geld_inst)
-			var geld_pos = position
-			geld_pos.x += 100
-			geld_inst.position = geld_pos
+#	if $VisibilityNotifier2D.is_on_screen():
+	var geld_inst = geld.instance()
+	var parent = get_parent()
+	if parent != null:
+		parent.add_child(geld_inst)
+		var geld_pos = position
+#		geld_pos.x += 100
+		geld_inst.position = geld_pos
