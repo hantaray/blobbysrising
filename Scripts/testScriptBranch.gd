ec1c9325c19f1f07a3fd7c7cbfb8169e4b4ed5de
Konne <konnevonronks@fmail.com>
2021-06-03 11:27:16 +0200

    neues test script

---
+++ Scripts/testScriptBranch.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var test = 1
+var test2 = "jbhasdi"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
