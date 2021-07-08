a4152206d6d54ef1b7405960e6a3751c2a646e56
Konne <konnevonronks@gmail.com>
2021-07-08 18:49:40 +0200

    func deleted

--- Scripts/Manual_Screen.gd
+++ Scripts/Manual_Screen.gd
@@ -1,21 +1,6 @@
 extends Control
 
-func _ready():
-	var currentLevel = MainScript.current_scene
-	var manual_container_path = "res://gameover_screen/ManualScreens/ManualContainer/ManualContainer_Peace.tscn"
-	if currentLevel == "Subway":
-		manual_container_path = "res://gameover_screen/ManualScreens/ManualContainer/ManualContainer_Train.tscn"
-	if currentLevel == "City":
-		manual_container_path = "res://gameover_screen/ManualScreens/ManualContainer/ManualContainer_City.tscn"
-	if currentLevel == "Street":
-		manual_container_path = "res://gameover_screen/ManualScreens/ManualContainer/ManualContainer_Bike.tscn"
-	if currentLevel == "Redis":
-		manual_container_path = "res://gameover_screen/ManualScreens/ManualContainer/ManualContainer_Umverteilung.tscn"
-	if currentLevel == "Peace":
-		manual_container_path = "res://gameover_screen/ManualScreens/ManualContainer/ManualContainer_Peace.tscn"
-	if currentLevel == "Caring":
-		manual_container_path = "res://gameover_screen/ManualScreens/ManualContainer/ManualContainer_Care.tscn"
-	$ManualScrollContainer/ManualCenterContainer.add_child(load(manual_container_path).instance())
+
 
 
 func _on_BackButton_pressed():
